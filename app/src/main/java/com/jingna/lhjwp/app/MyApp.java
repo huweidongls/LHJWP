4d9955522fd5485089368841ada44642f266015f
huweidong <466463054@qq.com>
2019-01-02 17:37:12 +0800

    123321

--- app/src/main/java/com/jingna/lhjwp/app/MyApp.java
+++ app/src/main/java/com/jingna/lhjwp/app/MyApp.java
@@ -42,7 +42,7 @@ public class MyApp extends Application {
         SDKInitializer.initialize(this);
         //自4.3.0起，百度地图SDK所有接口均支持百度坐标和国测局坐标，用此方法设置您使用的坐标类型.
         //包括BD09LL和GCJ02两种坐标，默认是BD09LL坐标。
-        SDKInitializer.setCoordType(CoordType.BD09LL);
+        SDKInitializer.setCoordType(CoordType.GCJ02);
         ScreenAdapterTools.init(this);
         ZXingLibrary.initDisplayOpinion(this);
         ViseHttp.init(this);
--- app/src/main/java/com/jingna/lhjwp/camera/PublicCameraView.java
+++ app/src/main/java/com/jingna/lhjwp/camera/PublicCameraView.java
@@ -686,7 +686,7 @@ public class PublicCameraView extends FrameLayout implements SurfaceHolder.Callb
         LocationClientOption option = new LocationClientOption();
         option.setLocationMode(LocationClientOption.LocationMode.Battery_Saving
         );//可选，默认高精度，设置定位模式，高精度，低功耗，仅设备
-        option.setCoorType("bd09ll");//可选，默认gcj02，设置返回的定位结果坐标系
+        option.setCoorType("gcj02");//可选，默认gcj02，设置返回的定位结果坐标系
         int span = 1000;
         option.setScanSpan(span);//可选，默认0，即仅定位一次，设置发起定位请求的间隔需要大于等于1000ms才是有效的
         option.setIsNeedAddress(true);//可选，设置是否需要地址信息，默认不需要
